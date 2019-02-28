053eb40fff25ca42a9936fd977ec9cfdc3dd4cca
suyatna <antasuy@gmail.com>
2019-02-28 14:49:19 +0700

    adding parceable

--- app/src/main/java/com/zuyatna/madedicoding/myintentapp/MainActivity.java
+++ app/src/main/java/com/zuyatna/madedicoding/myintentapp/MainActivity.java
@@ -1,13 +1,64 @@
 package com.zuyatna.madedicoding.myintentapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnMoveActivity;
+    Button btnMoveWithData;
+    Button btnMoveWithObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+
+        btnMoveWithData = findViewById(R.id.btn_move_activity_data);
+        btnMoveWithData.setOnClickListener(this);
+
+        btnMoveWithObject = findViewById(R.id.btn_move_activity_object);
+        btnMoveWithObject.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btn_move_activity:
+
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+            case R.id.btn_move_activity_data:
+
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Suyatna");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 21);
+
+                startActivity(moveWithDataIntent);
+                break;
+            case R.id.btn_move_activity_object:
+
+                Person person = new Person();
+                person.setName("Suyatna");
+                person.setAge(21);
+                person.setEmail("zuyatna@gmail.com");
+                person.setCity("Bekasi");
+
+                Intent moveWithObjectIntent = new Intent(MainActivity.this, MoveWithObjectActivity.class);
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person);
+
+                startActivity(moveWithObjectIntent);
+                break;
+        }
     }
 }
